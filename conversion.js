0e98eb0cd0652a8847e66362db6d0ef210758875
Sejal K. Parmar <sejalkp2@illinois.edu>
2017-12-07 17:11:14 -0600

    moved script to conversion.js and moved h1 up

--- conversion.js
+++ conversion.js
@@ -1,3 +1,12 @@
+function currConvert() {
+    var amount = prompt("Please enter the amount of USD you wish to convert. Don't include the $.", "10.00");
+    var currency = prompt("Please enter the currency you wish to convert into", "GBP");
+    var conversion = 0;
+    if (amount != null && currency != null) {
+        document.getElementById("convertedAmount").innerHTML =
+            "$ " + amount + " in USD is " + conversion + " in " + currency;
+    }
+}
 fx.rates = {
     GBP: 0.6,
     USD: 1
