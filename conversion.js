f8f594e5bebe6636c43891416928b43e15dbafa6
Sejal K. Parmar <sejalkp2@illinois.edu>
2017-12-07 17:14:06 -0600

    added option to exit conversion

--- conversion.js
+++ conversion.js
@@ -1,6 +1,9 @@
 function currConvert() {
     var amount = prompt("Please enter the amount of USD you wish to convert. Don't include the $.", "10.00");
-    var currency = prompt("Please enter the currency you wish to convert into", "GBP");
+    var currency = null;
+    if(amount != null) {
+        currency = prompt("Please enter the currency you wish to convert into", "GBP");
+    }
     var conversion = 0;
     if (amount != null && currency != null) {
         document.getElementById("convertedAmount").innerHTML =
