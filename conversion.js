e56bcfee605a4f30522792ac6fdd8b82487371ba
laraei2 <laraei2@illinois.edu>
2017-12-12 19:43:29 -0600

    done :-) enjoy @challen
    I hope no merge conflicts.

--- conversion.js
+++ conversion.js
@@ -181,5 +181,5 @@ function currConversion(curr, amt) {
         ZMW: 10.414584,
         ZWL: 322.355011
     };
-    return rates[curr] * amt;
+    return parseFloat(parseFloat(rates[curr] * amt).toFixed(2));
 }
